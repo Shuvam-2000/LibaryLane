84ad1dd819ff49df8419e4515fc786f46f2979c6
Shuvam-2000 <shuvamsaha2000@gmail.com>
2025-01-01 16:40:08 +0530

    added hero and havealook component to the home page

---
+++ Client/LibaryLane/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import HaveALook from "../components/HaveALook";
+import Hero from "../components/Hero";
+
+const Home = () => {
+  return (
+    <div>
+      <Hero />
+      <HaveALook />
+    </div>
+  )
+}
+
+export default Home;
