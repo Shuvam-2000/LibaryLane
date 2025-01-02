6fdfcd74f0c6db9f88932fceb65870c76234b8ae
Shuvam-2000 <shuvamsaha2000@gmail.com>
2025-01-02 16:48:21 +0530

    add: have a look section updated for showcsing the free books

--- Client/LibaryLane/src/components/HaveALook.jsx
+++ Client/LibaryLane/src/components/HaveALook.jsx
@@ -1,10 +1,18 @@
+import freecourses from '../public/freecourses.json';
 
 const HaveALook = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const filterBooks = freecourses.filter((data) => data.category === 'Free');
+  console.log(filterBooks);
 
-export default HaveALook
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center text-sm py-4 sm:border-none border-b border-gray-300">
+        <h1 className="text-center sm:text-2xl text-xl font-medium">
+          Have A Look<span className="text-red-500"> At Our Free Books</span>
+        </h1>
+      </div>
+    </>
+  );
+};
+
+export default HaveALook;
