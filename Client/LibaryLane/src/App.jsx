9863c17908134c72b2bfb4246a1912db766081a1
Shuvam-2000 <shuvamsaha2000@gmail.com>
2025-01-05 16:34:49 +0530

    update: signup, login and storypage component added

--- Client/LibaryLane/src/App.jsx
+++ Client/LibaryLane/src/App.jsx
@@ -5,6 +5,9 @@ import Footer from './components/Footer';
 import OurBooks from './pages/OurBooks';
 import ContactUs from './pages/ContactUs';
 import AboutUs from './pages/AboutUs';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
+import StoryPage from './pages/StoryPage';
 import './index.css';
 
 function App() {
@@ -18,6 +21,9 @@ function App() {
           <Route path="/ourbooks" element={<OurBooks />} />
           <Route path="/contactus" element={<ContactUs />} />
           <Route path="/aboutus" element={<AboutUs />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/story/:id" element={<StoryPage />} />
         </Routes>
         <Footer />
       </BrowserRouter>
