3be499aded35d36aa226c890236fdd1094dfde93
Shuvam-2000 <shuvamsaha2000@gmail.com>
2025-01-09 16:03:17 +0530

    Chore: added comments to the code for understability

--- Server/routes/bookRoute.js
+++ Server/routes/bookRoute.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import { getBookInfo } from '../controllers/bookController.js';
 
+// intialize router
 const router = express.Router();
 
+// new books route 
 router.get('/book', getBookInfo);
 
 export default router;
