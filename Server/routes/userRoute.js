ea5ea636b2c8a9d1587aa26da7bc34797a043c61
Shuvam-2000 <shuvamsaha2000@gmail.com>
2025-01-10 16:05:57 +0530

    feat: exisiting user login and logout routes added

--- Server/routes/userRoute.js
+++ Server/routes/userRoute.js
@@ -1,10 +1,16 @@
 import express from 'express';
-import { userSignUp } from '../controllers/userController.js';
+import { userLogin, userLogout, userSignUp } from '../controllers/userController.js';
 
 // intialize router
 const router = express.Router();
 
 // new user signup route
 router.post('/signup', userSignUp);
+
+// existing user login route
+router.post('/login', userLogin);
+
+// existing user logout route
+router.get('/logout', userLogout);
 
 export default router;
