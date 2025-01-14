973c52bba3955b7877912fb5776355177d8153e5
Shuvam-2000 <shuvamsaha2000@gmail.com>
2025-01-14 16:33:15 +0530

    feat: functionality added to collect contact form data from the frontend

---
+++ Server/controllers/contactController.js
@@ -0,0 +1,24 @@
+import ContactModel from "../models/contact.js";
+
+export const getContactInfo = async (req,res) => {
+    // fetch all required fields from the frontend
+    const { fullname, email, message } = req.body;  
+
+    // check all fields are given
+    if(!fullname || !email || !message) return res.status(403).json({ 
+        message: 'All Fields Required', 
+        success: false });
+    try {
+        // create a contact field with details in the db
+        const newMessage = new ContactModel({
+            fullname, 
+            email, 
+            message
+        });
+        await newMessage.save();
+        res.status(200).json({ message: 'Message Received', success: true });
+    } catch (error) {
+        console.error('Intenral Server Error', error);
+        res.status(500).json({ message: 'Intenral Server Error', success: false })
+    }
+}
