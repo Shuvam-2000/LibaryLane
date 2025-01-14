2614dc2109ce967c4befcca4eef6a21a8b841bf2
Shuvam-2000 <shuvamsaha2000@gmail.com>
2025-01-14 16:12:26 +0530

    feat: toast package intialized, fixed the rotuing for contact form, added scroll to top component

--- Client/LibaryLane/src/App.jsx
+++ Client/LibaryLane/src/App.jsx
@@ -1,4 +1,5 @@
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast'
 import Home from './pages/Home';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
@@ -9,18 +10,21 @@ import Login from './pages/Login';
 import SignUp from './pages/SignUp';
 import FreeBook from './pages/FreeBook';
 import PaidBook from './pages/PaidBook';
+import ScrollToTop from './components/ScrollToTop';
 import './index.css';
 
 function App() {
 
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[4vw]'>
+      <Toaster />
       <BrowserRouter>
         <Navbar />
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/ourbooks" element={<OurBooks />} />
-          <Route path="/contactus" element={<ContactUs />} />
+          <Route path="/contact" element={<ContactUs />} />
           <Route path="/aboutus" element={<AboutUs />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
